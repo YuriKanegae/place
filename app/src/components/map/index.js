15e2a394c3c0a8414b81b02a5e2e0f204508ada1
ykanegae <yurikanegae@gmail.com>
2025-08-07 20:33:44 -0300

    temp

--- app/src/components/map/index.js
+++ app/src/components/map/index.js
@@ -1,12 +1,12 @@
-import maplibregl from 'maplibre-gl';
+import maplibregl        from 'maplibre-gl';
+import * as MaplibreGrid from 'maplibre-grid';
 
-import * as styles from "./styles.json";
+import * as styles       from "./styles.json";
 
 class MapContainer extends HTMLElement {
     
     constructor() {
         super();    
-        
         this.setAttribute( "id", "map" );
     }
 
@@ -14,12 +14,30 @@ class MapContainer extends HTMLElement {
         const map = new maplibregl.Map({
             container:      'map',
             style:          styles,
-            center:         [0, 0],
-            zoom:           3,
+            center:         [ -48.20008964929622, -21.78521330761199 ],
+            zoom:           15,
             maplibreLogo:   false
         });
 
-        map.addControl(new maplibregl.FullscreenControl());
+        map.on('load', async () => this.configureMap(map));
+    }
+
+    async configureMap(map) {
+        this.loadLines(map);
+    }
+
+    async loadLines(map) {
+        const grid = new MaplibreGrid.Grid({
+            units:      'kilometers',
+            gridWidth:  0.015,
+            gridHeight: 0.015,
+            minZoom:    15,
+            paint: { 'line-opacity': 0.2 }
+        });
+
+        map.addControl(grid);
+        map.on(MaplibreGrid)
+        map.on(MaplibreGrid.GRID_CLICK_EVENT, event => { console.log(event); });
     }
 }
 
